3c566e35ae3754fc351e56c68cebb936646ef0a9
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-10-16 23:17:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function decimalToBinary(num) {
   //Write you code here
-	const binary = num.toString(2);
-	return parseInt(binary);
+	 // array to store binary number
+    let binaryNum = new Array(32);
+ 
+    // counter for binary array
+    let i = 0;
+    while (n > 0) {
+ 
+        // storing remainder in binary array
+        binaryNum[i] = n % 2;
+        n = Math.floor(n / 2);
+        i++;
+    } 
+	return ...binaryNum;
+	
 	
   
 }
